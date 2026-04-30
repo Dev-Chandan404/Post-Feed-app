f8fa445575a90afdbb1f677dff83b3977327465e
chandan <dev.chandankumar404@gmail.com>
2026-04-30 18:27:57 +0530

    first commit

---
+++ Frontend/src/pages/Feed.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
+const Feed = () => {
+    const [posts, setPosts] = useState([
+        {
+            _id: "1",
+            caption: "My first post",
+            image: "https://wallpapers.com/images/hd/iron-man-tony-stark-ouqxo5w2b59h0042.jpg",
+            caption: "Iron Man",
+        }
+    ])
+    useEffect(() => {
+
+        axios.get("http://localhost:3000/posts")
+            .then((res) => {
+                setPosts(res.data.posts)
+            })
+    }, [])
+    return (
+        <section className='feed-section'>
+            <h1>Feed</h1>
+            {
+                posts.length > 0 ? (
+                    posts.map((post) => (
+                        <div key={post._id}
+                            className='post-card'>
+                            <img src={post.image} alt={post.caption} />
+                            <p>{post.caption}</p>
+                        </div>
+                    ))
+                ) : (
+                    <h1>No posts available</h1>
+                )
+            }
+        </section>
+    )
+}
+export default Feed
